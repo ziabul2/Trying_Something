99839395c1cee1593c8892bbf5414bafd1798b34
ziabul2 <71348433+ziabul2@users.noreply.github.com>
2021-12-20 14:53:57 +0600

    main

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, jim");
-Console.WriteLine("Hello, Ziabul");
+
+int a = 12;
+int b = 10;
+int c;
+c = a + b;
+
+Console.Write("The sum of a + b = ");
+Console.WriteLine(c);
+
+Console.WriteLine("Press Enter for Exit...");
 Console.ReadLine();
